4b104fa8040870565a4cab36e61127d5230345f9
timneumann1 <tim_neumann@gwu.edu>
2025-05-12 03:17:07 +0200

    Backup of exec

--- exec.py
+++ exec.py
@@ -1,28 +1,33 @@
 import subprocess
-'''
+
+
 ##### B ########
-B_values = [1e-4, 1e-3, 1e-2, 1e-1]
+
+# NOTE: The hyperparameter B here refers to the hyperparameter B' in the report
+
+B_values = [0, 1e-6, 1e-4, 1e-2]
 base_args = [
     "--hamiltonian_label", "heisenberg",
-    "--n_qubits", "8",
+    "--n_qubits", "4",
     "--rows", "2",
-    "--cols", "4"
+    "--cols", "2"
 ]
 
 for B in B_values:
     print(f"\n=== Running with B = {B} ===")
-    args = ["python", "main.py"] + base_args + ["--B", str(B)]
+    args = ["python", "main.py"] + base_args + ["--B", str(B)] + ["--analytical", str(True)]
+    print(args)
     subprocess.run(args)
-    
-'''
+
+
 ##### Analytical ########
 print(f"\n=== Running with analytic=True ===")
 
 base_args = [
     "--hamiltonian_label", "heisenberg",
-    "--n_qubits", "6",
+    "--n_qubits", "10",
     "--rows", "2",
-    "--cols", "3",
+    "--cols", "5",
     "--B", "1e-4"
 ]
 
@@ -32,17 +37,16 @@ subprocess.run(args)
 '''
 ##### Data Generation #####
 
-hamiltonian_labels = ['heisenberg', 'antiferro_XY', 'z', 'ising']
+hamiltonian_labels = ['antiferro_XY', 'z', 'ising']
 base_args = [
-    "--B", "1e-4"
+    "--B", "1e-6",
+    "--n_qubits", "10",
+    "--rows", "2",
+    "--cols", "5"
 ]
 
 for hamiltonian in hamiltonian_labels:
-    for lattice in [[2,5],[2,6]]:
-        rows = lattice[0]
-        cols = lattice[1]
-        n_qubits = rows * cols
-        print(f"\n=== Running with Hamiltonian = {hamiltonian} === on {rows}x{cols} lattice ===")
-        args = ["python", "main.py"] + base_args + ["--n_qubits", str(n_qubits)] + ["--hamiltonian_label", hamiltonian] + ["--rows", str(rows)] + ["--cols", str(cols)]
-        subprocess.run(args)
+    print(f"\n=== Running with Hamiltonian = {hamiltonian} === on {rows}x{cols} lattice ===")
+    args = ["python", "main.py"] + base_args + ["--hamiltonian_label", hamiltonian]
+    subprocess.run(args)
 '''
